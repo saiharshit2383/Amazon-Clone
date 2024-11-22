01b23fb7510a677aea12d5f79514b7b4d48bdb80
saiharshit2383 <112844675+saiharshit2383@users.noreply.github.com>
2024-11-22 09:53:23 +0530

    Delete scripts directory

--- scripts/checkout.js
+++
@@ -1,4 +0,0 @@
-import { renderOrderSummary } from "./checkout/orderSummary.js";
-import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-renderOrderSummary();
-renderPaymentSummary();
